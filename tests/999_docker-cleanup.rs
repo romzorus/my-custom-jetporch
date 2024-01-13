e2ff1c17642e90b8060746956610f72ca7f360e7
Romzorus <romzorus591@gmail.com>
2024-01-13 22:39:52 +0100

    Adding a cleanup "test" which run the corresponding script. Its name starts with 999 in order to be launched last by cargo, assuming the naming discipline is enforced.

---
+++ tests/999_docker-cleanup.rs
@@ -0,0 +1,19 @@
+use std::process::Command;
+
+// This 'test' is just about running the cleanup script
+// named '999_docker-cleanup-script.sh' and using its exit code as a test result.
+// If the script fails, the test fails -> cleanup failed
+#[test]
+fn docker_init() {
+    let mut command = Command::new("tests/999_docker-cleanup-script.sh")
+        .spawn()
+        .expect("Problem launching the cleanup script");
+
+    let results = command
+        .wait()
+        .expect("Something went wrong with the cleanup script itself");
+
+    if !results.success() {
+        panic!("Something went wrong with the cleanup script itself");
+    }
+}
