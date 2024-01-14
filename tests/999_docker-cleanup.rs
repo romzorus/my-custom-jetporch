6fb77d84eb4207625e103bc49ef3a42fcf859ea1
Romzorus <romzorus591@gmail.com>
2024-01-14 01:40:55 +0100

    Important typo fix

--- tests/999_docker-cleanup.rs
+++ tests/999_docker-cleanup.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 // named '999_docker-cleanup-script.sh' and using its exit code as a test result.
 // If the script fails, the test fails -> cleanup failed
 #[test]
-fn docker_init() {
+fn docker_cleanup() {
     let mut command = Command::new("tests/999_docker-cleanup-script.sh")
         .spawn()
         .expect("Problem launching the cleanup script");
