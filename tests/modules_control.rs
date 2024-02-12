6b0a3a464cb7507f0d34d3b8a2a92289e9dbcdd2
Romzorus <romzorus591@gmail.com>
2024-02-13 00:22:34 +0100

    Adding tests for assert and fail modules

--- tests/modules_control.rs
+++ tests/modules_control.rs
@@ -68,3 +68,126 @@ fn test_module_echo_facts_debug() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(output.status.success(), true);
     Ok(())
 }
+
+#[test]
+fn test_module_fail() -> Result<(), Box<dyn std::error::Error>> {
+    // Creating a temporary folder to work in
+    let tempfolder = TempDir::new()?;
+
+    docker_init("containers-list-mod-fail.json", "fail");
+    create_inventory(&tempfolder, "containers-list-mod-fail.json");
+
+    // Fails
+    let playbookcontent =
+r#"---
+- name: fail module testing
+  groups:
+    - all
+
+  tasks:
+
+  - !fail
+    name: this will not fail
+    aftertask:
+      ignore_errors: true
+
+  - !fail
+    name: this will fail
+"#;
+
+    create_playbook(&tempfolder, playbookcontent);
+    
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+    cmd.arg("ssh")
+        .arg("-p")
+        .arg(format!("{}/playbooks/play.yml", tempfolder.path().display()))
+        .arg("-i")
+        .arg(format!("{}/inventory", tempfolder.path().display()))
+        .arg("-u")
+        .arg("root");
+
+    let output = cmd.output().unwrap();
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
+    docker_cleanup("containers-list-mod-fail.json", "fail");
+
+    assert_eq!(output.status.success(), false);
+    Ok(())
+}
+
+#[test]
+fn test_module_assert() -> Result<(), Box<dyn std::error::Error>> {
+    // Creating a temporary folder to work in
+    let tempfolder = TempDir::new()?;
+
+    docker_init("containers-list-mod-assert.json", "assert");
+    create_inventory(&tempfolder, "containers-list-mod-assert.json");
+
+    // Fails
+    let playbookcontent =
+r#"---
+- name: assert module testing
+  groups:
+    - all
+
+  defaults:
+    dog: scooby
+    ghost: "blinky"
+
+  tasks:
+
+  - !facts
+
+  - !assert
+    name: test1
+    msg: the OS must be Linux
+    true: (eq jet_os_type "Linux")
+       
+  - !assert
+    name: test2
+    msg: the OS must not be MacOS
+    false: (eq jet_os_type "MacOS")
+
+  - !assert
+    name: test3
+    msg: various things must all be true
+    all_true:
+      - (eq dog "scooby")
+      - (eq ghost "blinky")
+          
+  - !assert
+    name: test4
+    msg: none of these things may be true
+    all_false:
+      - (eq jet_os_type "Atari")
+      - (eq ghost "Slimer")
+          
+  - !assert
+    name: test5
+    msg: one of these things must be true
+    some_true:
+      - (eq ghost "Slimer")
+      - (eq jet_os_type "Linux")
+"#;
+
+    create_playbook(&tempfolder, playbookcontent);
+    
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+    cmd.arg("ssh")
+        .arg("-p")
+        .arg(format!("{}/playbooks/play.yml", tempfolder.path().display()))
+        .arg("-i")
+        .arg(format!("{}/inventory", tempfolder.path().display()))
+        .arg("-u")
+        .arg("root");
+
+    let output = cmd.output().unwrap();
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
+    docker_cleanup("containers-list-mod-assert.json", "assert");
+
+    assert_eq!(output.status.success(), true);
+    Ok(())
+}
